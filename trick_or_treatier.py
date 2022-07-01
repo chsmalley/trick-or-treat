aac0a7d77824557073072f341dc9a8d3a879eefe
Chris Smalley <chris.smalley@toyota.com>
2022-07-01 13:32:43 -0400

    mid progress updates

--- trick_or_treatier.py
+++ trick_or_treatier.py
@@ -2,26 +2,33 @@ import sys
 from gpiozero import Motor, Button, LED
 import time
 import sphero_mini
+import random
 
 # CONSTANTS
-CANDY_TIME = 2
+TREAT_TIME = 2
 BUBBLE_TIME = 2
-ROLL_TIME = 10
-ROLL_STEP_TIME = 0.5
+ROLL_TIME = 5
+ROLL_STEP_TIME = 0.1
 SPHERO_SPEED = 100  # Int 0 - 255
+TRICKS = ("BUBBLE", "BIRD", "SPHERO")
+# GPIO PINS
+TREAT_BUTTON_PIN = 2
+BUBBLE_BUTTON_PIN = 3
+TREAT_LED_PIN = 20
+BUBBLE_LED_PIN = 21
 
 # Trick or Treat object to handle devices
 class TrickOrTreat():
     def __init__(self, sphero_mac: str):
         self.running = False
         # Setup motors
-        self.candy_motor = Motor(forward=4, backward=14)
+        self.treat_motor = Motor(forward=4, backward=14)
         self.bubble_motor = Motor(forward=17, backward=27)
         # Setup buttons
-        self.candy_button = Button(2)
-        self.bubble_button = Button(3)
+        self.treat_button = Button(TREAT_BUTTON_PIN)
+        self.bubble_button = Button(BUBBLE_BUTTON_PIN)
         # Setup LEDs
-        self.candy_led = LED(20)
+        self.treat_led = LED(20)
         self.bubble_led = LED(21)
         # Setup sphero ball
         self.sphero = sphero_mini.sphero_mini(sphero_mac)
@@ -42,25 +49,39 @@ class TrickOrTreat():
         self.sphero.roll(0, 0)
         self.sphero.wait(1)
         self.sphero.setLEDColor(red=0, green=0, blue=255)
+        
+    def _treat(self):
+        self.treat_led.on()
+        self.treat_motor.forward()
+        time.sleep(TREAT_TIME)
+        # self.treat_motor.backward()
+        # time.sleep(TREAT_TIME)
+        # self.treat_motor.forward()
+        # time.sleep(TREAT_TIME)
+        self.treat_motor.stop()
+        self.treat_led.off()
     
     def run(self):
         self.running = True
-        while True:
-            if self.candy_button.is_pressed:
-                print("candy button pressed")
-                self.candy_motor.forward()
-                self.candy_led.on()
-                time.sleep(CANDY_TIME)
-                self.candy_motor.stop()
-                self.candy_led.off()
+        while self.running:
+            if self.treat_button.is_pressed:
+                print("treat button pressed")
+                self._treat()
             elif self.bubble_button.is_pressed:
                 print("bubble button pressed")
+                trick = random.choice(TRICKS)
+                if trick == "BUBBLE":
+                    self._bubble_trick()
+                elif trick == "SPHERO":
+                    self._sphero_trick()
+                else:
+                    print(f"Unknown trick: {trick}")
             else:
                 # Don't run too fast
                 time.sleep(0.01)
 
     def stop(self):
-        pass
+        self.running = False
 
 if __name__ == '__main__':
     sphero_mac = sys.argv[1]
