6ce8deef57c9fc9a89aaf50f7d57e90298d4558c
Chris Smalley <chris.smalley@toyota.com>
2022-07-17 21:41:13 -0400

    moved treats into threads

--- trick_or_treatier.py
+++ trick_or_treatier.py
@@ -3,6 +3,8 @@ from gpiozero import Motor, Button, LED, DigitalOutputDevice
 import time
 import sphero_mini
 import random
+import threading
+import queue
 
 # CONSTANTS
 TREAT_TIME = 2
@@ -18,7 +20,7 @@ TRICKS = [
     "BUBBLE",
     "BIRD",
     "LADDER",
-    "LIGHTS",
+    # "SPHERO"
 ]
 # GPIO PINS
 TREAT_BUTTON_PIN = 2
@@ -29,8 +31,8 @@ TREAT_MOTOR_FORWARD_PIN = 23
 TREAT_MOTOR_BACKWARD_PIN = 24
 BUBBLE_MOTOR_FORWARD_PIN = 27
 BUBBLE_MOTOR_BACKWARD_PIN = 17
-LADDER_PIN = 26
-LIGHTS_PIN = 19
+LADDER_PIN_ON = 26
+LADDER_PIN_OFF = 19
 BIRD_PIN = 13
 CAR_FORWARD_PIN = 6
 CAR_BACKWARD_PIN = 5
@@ -39,6 +41,8 @@ CAR_BACKWARD_PIN = 5
 class TrickOrTreat():
     def __init__(self, sphero_mac: str):
         self.running = False
+        self.trick_queue = queue.Queue()
+        self.treat_queue = queue.Queue()
         # Setup motors
         self.treat_motor = Motor(forward=TREAT_MOTOR_FORWARD_PIN,
                                  backward=TREAT_MOTOR_BACKWARD_PIN)
@@ -57,12 +61,37 @@ class TrickOrTreat():
         else:
             self.sphero = None
         # Setup other tricks
-        self.jacobs_ladder = DigitalOutputDevice(LADDER_PIN)
-        self.lights = DigitalOutputDevice(LIGHTS_PIN)
+        self.jacobs_ladder_on = DigitalOutputDevice(LADDER_PIN_ON)
+        self.jacobs_ladder_off = DigitalOutputDevice(LADDER_PIN_OFF)
         self.bird = DigitalOutputDevice(BIRD_PIN)
         self.car_forward = DigitalOutputDevice(CAR_FORWARD_PIN)
         self.car_backward = DigitalOutputDevice(CAR_BACKWARD_PIN)
-
+        # Setup tricks thread
+        self.trick_thread = threading.Thread(target=self._handle_tricks)
+        # Setup treats thread
+        self.treat_thread = threading.Thread(target=self._handle_treats)
+        
+    def _handle_treats(self):
+        while self.running:
+            treat = self.treat_queue.get()
+            if treat == "CANDY":
+                self._treat()
+        
+    def _handle_tricks(self):
+        while self.running:
+            trick = self.trick_queue.get()
+            print(f"trick button pressed. Performing trick: {trick}")
+            if trick == "BUBBLE":
+                self._bubble_trick()
+            elif trick == "SPHERO":
+                self._sphero_trick()
+            elif trick == "LADDER":
+                self._ladder_trick()
+            elif trick == "BIRD":
+                self._bird_trick()
+            else:
+                print(f"Unknown trick: {trick}")
+            
     def _bubble_trick(self):
         self.bubble_motor.forward()
         self.bubble_led.on()
@@ -71,9 +100,13 @@ class TrickOrTreat():
         self.bubble_led.off()
 
     def _ladder_trick(self):
-        self.jacobs_ladder.on()
+        self.jacobs_ladder_on.on()
+        time.sleep(BUTTON_PRESS_DELAY)
+        self.jacobs_ladder_on.off()
         time.sleep(LADDER_TIME)
-        self.jacobs_ladder.off()
+        self.jacobs_ladder_off.on()
+        time.sleep(BUTTON_PRESS_DELAY)
+        self.jacobs_ladder_off.off()
 
     def _lights_trick(self):
         self.lights.on()
@@ -133,25 +166,15 @@ class TrickOrTreat():
     
     def run(self):
         self.running = True
+        # Start threads
+        self.trick_thread.start()
+        self.treat_thread.start()
         while self.running:
             if self.treat_button.is_pressed:
                 print("treat button pressed")
-                self._treat()
+                self.treat_queue.put("CANDY")
             elif self.bubble_button.is_pressed:
-                trick = random.choice(TRICKS)
-                print(f"trick button pressed. Performing trick: {trick}")
-                if trick == "BUBBLE":
-                    self._bubble_trick()
-                elif trick == "SPHERO":
-                    self._sphero_trick()
-                elif trick == "LADDER":
-                    self._ladder_trick()
-                elif trick == "LIGHTS":
-                    self._lights_trick()
-                elif trick == "BIRD":
-                    self._bird_trick()
-                else:
-                    print(f"Unknown trick: {trick}")
+                self.trick_queue.put(random.choice(TRICKS))
             else:
                 # Don't run too fast
                 time.sleep(0.01)
