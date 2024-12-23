85000ac4f2bdb40522d8ef0b963d6e3f42f0dc67
Chris Smalley <chris.smalley.32@gmail.com>
2024-12-23 14:47:17 -0500

    minor updates

--- nerf_shooter.py
+++ nerf_shooter.py
@@ -1,7 +1,13 @@
-from gpiozero import Motor
+from gpiozero import Motor, Button
 import time
 
 # CONSTANTS
+BARREL_SPEED = 0.5
+TRIGGER_SPEED = 0.5
+TRIGGER_DELAY = 0.2
+TRIGGER_TIME = 0.5
+PITCH_SPEED = 0.5
+PITCH_TIME = 0.5
 
 # GPIO PINS
 TRIGGER_MOTOR_FORWARD_PIN = 17
@@ -19,8 +25,17 @@ JAM_PIN = 21
 
 
 
-def shoot_dart(trigger: Motor, barrel: Motor):
-    
+def shoot_dart(trigger: Motor, barrel: Motor) -> None:
+    barrel.forward(BARREL_SPEED)
+    time.sleep(TRIGGER_DELAY)
+    trigger.forward(TRIGGER_SPEED)
+    time.sleep(TRIGGER_TIME)
+    trigger.stop()
+    barrel.stop()
+
+
+# def raise_motor(pitch: Motor, max_pitch: Button) -> None:
+#     while not 
 
 if __name__ == '__main__':
     # Create Motor objects
@@ -30,7 +45,9 @@ if __name__ == '__main__':
                          backward=BARREL_MOTOR_BACKWARD_PIN)
     pitch_motor = Motor(forward=PITCH_MOTOR_FORWARD_PIN,
                         backward=PITCH_MOTOR_FORWARD_PIN)
-
+    max_pitch = Button(MAX_PITCH_PIN)
+    min_pitch = Button(MIN_PITCH_PIN)
+    
     print("Nerf shooter")
     shoot_dart()
     print("\ngoodbye")
