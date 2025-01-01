2bd4f518835e1ef577eda2c812de0bf9cd36e17a
chris smalley <chris.smalley.32@gmail.com>
2025-01-01 13:57:49 -0500

    removed duplicate now that it is working

--- nerf/client.js
+++ nerf/client.js
@@ -10,7 +10,6 @@ async function shoot() {
 
 function negotiate() {
     pc.addTransceiver('video', {direction: 'recvonly'});
-    pc.addTransceiver('audio', {direction: 'recvonly'});
     return pc.createOffer().then(function(offer) {
         return pc.setLocalDescription(offer);
     }).then(function() {
@@ -49,29 +48,42 @@ function negotiate() {
     });
 }
 
-function start() {
+async function start() {
     var config = {
         sdpSemantics: 'unified-plan'
     };
 
-    if (document.getElementById('use-stun').checked) {
-        config.iceServers = [{urls: ['stun:stun.l.google.com:19302']}];
-    }
-
     pc = new RTCPeerConnection(config);
 
-    // connect audio / video
+    // connect video
     pc.addEventListener('track', function(evt) {
         if (evt.track.kind == 'video') {
             document.getElementById('video').srcObject = evt.streams[0];
-        } else {
-            document.getElementById('audio').srcObject = evt.streams[0];
         }
     });
-
+    
     document.getElementById('start').style.display = 'none';
     negotiate();
     document.getElementById('stop').style.display = 'inline-block';
+
+
+    // const pc = new RTCPeerConnection();
+    // pc.addTransceiver('video', { direction: 'recvonly' });
+
+    // const offer = await pc.createOffer();
+    // await pc.setLocalDescription(offer);
+
+    // const response = await fetch('/offer', {
+    //     method: 'POST',
+    //     body: JSON.stringify(pc.localDescription)
+    // });
+
+    // const answer = await response.json();
+    // await pc.setRemoteDescription(answer);
+
+    // pc.ontrack = function(event) {
+    //     document.getElementById('video').srcObject = event.streams[0];
+    // };
 }
 
 function stop() {
@@ -83,3 +95,7 @@ function stop() {
         pc.close();
     }, 500);
 }
+
+// document.getElementById('shoot').addEventListener('click', shoot);
+// document.getElementById('start').addEventListener('click', start);
+// window.onload = startStream;
--- nerf/nerf_app.py
+++ nerf/nerf_app.py
@@ -1,26 +1,26 @@
-import asyncio
-import json
-import logging
 import os
+import cv2
 import threading
+import json
+import asyncio
 from collections import OrderedDict
-
-from nerf_shooter import main as nerf_main
 import picamera
-from aiohttp import web
-from aiortc import RTCSessionDescription  # , RTCPeerConnection
-from aiortc.contrib.media import MediaPlayer
-from aiortc.rtcrtpparameters import RTCRtpCodecCapability
-from pitrack import H264EncodedStreamTrack
-from rtcpeerconnection import RTCPeerConnection
 
-logger = logging.getLogger(__name__)
+from aiohttp import web
+# from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
+from aiortc import RTCSessionDescription, VideoStreamTrack
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
+from nerf_shooter import main as nerf_main
+from pitrack import H264EncodedStreamTrack
+from aiortc.rtcrtpparameters import RTCRtpCodecCapability
+from rtcpeerconnection import RTCPeerConnection
+# from rtcrtpsender import RTCRtpSender
+
 
 FRAME_RATE = 30
 CAMERA_RESOLUTION = (640, 480)
 BASE_PATH = os.path.dirname(__file__)
 
-audio = None
 camera = None
 
 codec_parameters = OrderedDict(
@@ -36,16 +36,26 @@ pi_capability = RTCRtpCodecCapability(
 preferences = [pi_capability]
 pcs = set()
 
+class CameraStreamTrack(VideoStreamTrack):
+    def __init__(self):
+        super().__init__()
+        self.cap = cv2.VideoCapture(0)
+        
+    async def recv(self):
+        pts, time_base = await self.next_timestamp()
+        ret, frame = self.cap.read()
+        if not ret:
+            return
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        return VideoFrame.from_ndarray(frame, format="rgb24")
 
 async def index(request):
-    content = open(os.path.join(BASE_PATH, "index.html"), "r").read()
-    return web.Response(content_type="text/html", text=content)
-
+    content = open('index2.html', 'r').read()
+    return web.Response(content_type='text/html', text=content)
 
 async def javascript(request):
-    content = open(os.path.join(BASE_PATH, "client.js"), "r").read()
+    content = open(os.path.join(BASE_PATH, "client2.js"), "r").read()
     return web.Response(content_type="application/javascript", text=content)
-
 
 async def shoot(request):
     print("running shoot")
@@ -53,9 +63,8 @@ async def shoot(request):
     script_thread.start()
     return web.json_response({"status": "shooting nerf dart..."})
 
-
 async def offer(request):
-    global audio, camera
+    global camera
     params = await request.json()
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
 
@@ -75,16 +84,6 @@ async def offer(request):
         sei=False,
     )
 
-    # Read audio stream from `hw:1,0` (via alsa, from card1, device0. see output from `arecord -l`)
-    # Please check default Capture volume by `amixer`. You can set Capture volume by `amixer sset 'Capture' 80%`
-    try:
-        audio = MediaPlayer(
-            "hw:1,0", format="alsa", options={"channels": "1", "sample_rate": "44100"}
-        )
-    except:
-        print("Could not open or read audio device.")
-        audio = None
-
     pc = RTCPeerConnection()
     pcs.add(pc)
 
@@ -97,21 +96,17 @@ async def offer(request):
 
     await pc.setRemoteDescription(offer)
     for t in pc.getTransceivers():
-        if t.kind == "audio" and audio and audio.audio:
-            pc.addTrack(audio.audio)
         if t.kind == "video" and video_track:
             t.setCodecPreferences(preferences)
             pc.addTrack(video_track)
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
-    logger.info(answer)
     return web.Response(
         content_type="application/json",
         text=json.dumps(
             {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
         ),
     )
-
 
 async def on_shutdown(app):
     global camera
@@ -122,15 +117,13 @@ async def on_shutdown(app):
     pcs.clear()
     camera.stop_recording()
     camera.close()
-    audio.audio.stop()
 
 
 if __name__ == "__main__":
-    ssl_context = None
     app = web.Application()
-    app.on_shutdown.append(on_shutdown)
-    app.router.add_get("/", index)
-    app.router.add_get("/client.js", javascript)
-    app.router.add_post("/offer", offer)
-    app.router.add_post("/shoot", shoot)
-    web.run_app(app, host="0.0.0.0", port=8080, ssl_context=ssl_context)
+    app.router.add_get('/', index)
+    app.router.add_get("/client2.js", javascript)
+    app.router.add_post('/offer', offer)
+    app.router.add_post('/shoot', shoot)
+
+    web.run_app(app, port=8080)
