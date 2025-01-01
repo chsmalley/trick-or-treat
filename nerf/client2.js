d43a99a62e620f7b1db6909e13ae634db73b0d81
Chris Smalley <chris.smalley.32@gmail.com>
2024-12-31 22:24:38 -0500

    bug fix

--- nerf/client2.js
+++ nerf/client2.js
@@ -49,41 +49,41 @@ function negotiate() {
 }
 
 async function startStream() {
-    var config = {
-        sdpSemantics: 'unified-plan'
-    };
-
-    pc = new RTCPeerConnection(config);
-
-    // connect audio / video
-    pc.addEventListener('track', function(evt) {
-        if (evt.track.kind == 'video') {
-            document.getElementById('video').srcObject = evt.streams[0];
-        } else {
-            document.getElementById('audio').srcObject = evt.streams[0];
-        }
-    });
-    
-    negotiate();
-
-
-    // const pc = new RTCPeerConnection();
-    // pc.addTransceiver('video', { direction: 'recvonly' });
-
-    // const offer = await pc.createOffer();
-    // await pc.setLocalDescription(offer);
-
-    // const response = await fetch('/offer', {
-    //     method: 'POST',
-    //     body: JSON.stringify(pc.localDescription)
-    // });
-
-    // const answer = await response.json();
-    // await pc.setRemoteDescription(answer);
-
-    // pc.ontrack = function(event) {
-    //     document.getElementById('video').srcObject = event.streams[0];
+    // var config = {
+    //     sdpSemantics: 'unified-plan'
     // };
+
+    // pc = new RTCPeerConnection(config);
+
+    // // connect audio / video
+    // pc.addEventListener('track', function(evt) {
+    //     if (evt.track.kind == 'video') {
+    //         document.getElementById('video').srcObject = evt.streams[0];
+    //     } else {
+    //         document.getElementById('audio').srcObject = evt.streams[0];
+    //     }
+    // });
+    
+    // negotiate();
+
+
+    const pc = new RTCPeerConnection();
+    pc.addTransceiver('video', { direction: 'recvonly' });
+
+    const offer = await pc.createOffer();
+    await pc.setLocalDescription(offer);
+
+    const response = await fetch('/offer', {
+        method: 'POST',
+        body: JSON.stringify(pc.localDescription)
+    });
+
+    const answer = await response.json();
+    await pc.setRemoteDescription(answer);
+
+    pc.ontrack = function(event) {
+        document.getElementById('video').srcObject = event.streams[0];
+    };
 }
 
 document.getElementById('shootButton').addEventListener('click', shoot);
