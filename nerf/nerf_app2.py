d11fd67bdd40b513eb76963f38488b083275acd0
Chris Smalley <chris.smalley.32@gmail.com>
2024-12-31 22:35:03 -0500

    bug fix

--- nerf/nerf_app2.py
+++ nerf/nerf_app2.py
@@ -2,12 +2,36 @@ import os
 import cv2
 import threading
 import asyncio
+from collections import OrderedDict
+import picamera
+
 from aiohttp import web
 from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
 from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
 from nerf_shooter import main as nerf_main
+from pitrack import H264EncodedStreamTrack
+from aiortc.rtcrtpparameters import RTCRtpCodecCapability
+from rtcrtpsender import RTCRtpSender
 
+FRAME_RATE = 30
+CAMERA_RESOLUTION = (640, 480)
 BASE_PATH = os.path.dirname(__file__)
+
+audio = None
+camera = None
+
+codec_parameters = OrderedDict(
+    [
+        ("packetization-mode", "1"),
+        ("level-asymmetry-allowed", "1"),
+        ("profile-level-id", "42001f"),
+    ]
+)
+pi_capability = RTCRtpCodecCapability(
+    mimeType="video/H264", clockRate=90000, channels=None, parameters=codec_parameters
+)
+preferences = [pi_capability]
+pcs = set()
 
 class CameraStreamTrack(VideoStreamTrack):
     def __init__(self):
@@ -37,25 +61,61 @@ async def shoot(request):
     return web.json_response({"status": "shooting nerf dart..."})
 
 async def offer(request):
+    global audio, camera
     params = await request.json()
-    offer = RTCSessionDescription(sdp=params['sdp'], type=params['type'])
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    video_track = H264EncodedStreamTrack(FRAME_RATE)
+    if not camera:
+        camera = picamera.PiCamera()
+        camera.resolution = CAMERA_RESOLUTION
+        camera.framerate = FRAME_RATE
+    else:
+        camera.stop_recording()
+
+    camera.start_recording(
+        video_track,
+        format="h264",
+        profile="constrained",
+        inline_headers=True,
+        sei=False,
+    )
+
+    # Read audio stream from `hw:1,0` (via alsa, from card1, device0. see output from `arecord -l`)
+    # Please check default Capture volume by `amixer`. You can set Capture volume by `amixer sset 'Capture' 80%`
+    try:
+        audio = MediaPlayer(
+            "hw:1,0", format="alsa", options={"channels": "1", "sample_rate": "44100"}
+        )
+    except:
+        print("Could not open or read audio device.")
+        audio = None
 
     pc = RTCPeerConnection()
-    pc.addTrack(CameraStreamTrack())
+    pcs.add(pc)
 
-    @pc.on("icegatheringstatechange")
-    async def on_icegatheringstatechange(event):
-        if pc.iceGatheringState == "complete":
-            iceGatheringCompleted.set()
+    @pc.on("iceconnectionstatechange")
+    async def on_iceconnectionstatechange():
+        print("ICE connection state is %s" % pc.iceConnectionState)
+        if pc.iceConnectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
 
     await pc.setRemoteDescription(offer)
+    for t in pc.getTransceivers():
+        if t.kind == "audio" and audio and audio.audio:
+            pc.addTrack(audio.audio)
+        if t.kind == "video" and video_track:
+            t.setCodecPreferences(preferences)
+            pc.addTrack(video_track)
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
-    
-    return web.json_response({
-        'sdp': pc.localDescription.sdp,
-        'type': pc.localDescription.type
-    })
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps(
+            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+        ),
+    )
 
 
 if __name__ == "__main__":
